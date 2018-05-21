eea2627e0f0554ba53c0d8842af51e7bd45c7d78
Simo-Pekka Kerkelä <simopekka1990@gmail.com>
2018-05-21 23:21:32 +0300

    more tests

--- lorem.go
+++ lorem.go
@@ -7,17 +7,27 @@ import (
 	"strings"
 )
 
-func main() {
-	loremWords := strings.Fields("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-	loremWordsCount := len(loremWords)
-	wordCountPtr := flag.Int("words", loremWordsCount, "number of words")
-	flag.Parse()
+var wordCount int
+var loremWords []string
 
+func init() {
+	loremWords = strings.Fields("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+	loremWordsCount := len(loremWords)
+	flag.IntVar(&wordCount, "words", loremWordsCount, "number of words")
+}
+
+func main() {
+	flag.Parse()
+	printLoremIpsum()
+}
+
+func printLoremIpsum() {
+	loremWordsCount := len(loremWords)
 	w := bufio.NewWriter(os.Stdout)
-	for i := 0; i < *wordCountPtr; i++ {
+	for i := 0; i < wordCount; i++ {
 		index := i % loremWordsCount
 		w.WriteString(loremWords[index])
-		if i != *wordCountPtr-1 {
+		if i != wordCount-1 {
 			w.WriteString(" ")
 		} else {
 			w.WriteString("\n")
--- lorem_test.go
+++ lorem_test.go
@@ -1,7 +1,37 @@
 package main
 
+import (
+	"flag"
+	"os"
+)
+
 func Example() {
 	main()
 	// Output:
 	// Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+}
+
+func Example_args1() {
+	os.Args = []string{"noop", "-words=2"}
+	flag.Parse()
+
+	printLoremIpsum()
+	// Output:
+	// Lorem ipsum
+}
+func Example_args2() {
+	os.Args = []string{"noop", "-words", "3"}
+	flag.Parse()
+
+	printLoremIpsum()
+	// Output:
+	// Lorem ipsum dolor
+}
+func Example_args3() {
+	os.Args = []string{"noop", "-words", "70"}
+	flag.Parse()
+
+	printLoremIpsum()
+	// Output:
+	// Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem
 }
