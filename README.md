8851474ac83b804d92f516ced1c80231f94e8414
spkerkela <simo-pekka.kerkela@reaktor.com>
2018-05-21 17:50:23 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Lorem ipsum generator
+
+## Installation
+```bash
+$ go get github.com/spkerkela/lorem
+```
+
+## Usage
+
+```bash
+$ lorem -h
+Usage of lorem:
+  -words int
+        number of words (default 69)
+```
---
+++ lorem.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	loremWords := strings.Fields("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+	loremWordsCount := len(loremWords)
+	wordCountPtr := flag.Int("words", loremWordsCount, "number of words")
+	flag.Parse()
+
+	if *wordCountPtr > loremWordsCount {
+		w := bufio.NewWriter(os.Stdout)
+		for i := 0; i < *wordCountPtr; i++ {
+			index := i % loremWordsCount
+			w.WriteString(loremWords[index])
+			if i != *wordCountPtr-1 {
+				w.WriteString(" ")
+			} else {
+				w.WriteString("\n")
+			}
+		}
+
+		w.Flush()
+	} else {
+		fmt.Println(strings.Join(loremWords[:*wordCountPtr], " "))
+	}
+
+}
