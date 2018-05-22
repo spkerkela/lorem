7ed1e8f24b179dd7f2cce992447096ae2b7ba50c
Simo-Pekka Kerkelä <simopekka1990@gmail.com>
2018-05-22 21:11:16 +0300

    simplify

--- lorem.go
+++ lorem.go
@@ -10,12 +10,7 @@ import (
 func main() {
 	var wordCount int
 	if len(os.Args) > 1 {
-		count, err := strconv.Atoi(os.Args[1])
-		if err != nil {
-			wordCount = 0
-		} else {
-			wordCount = count
-		}
+		wordCount, _ = strconv.Atoi(os.Args[1])
 	}
 
 	loremWords := strings.Fields("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
